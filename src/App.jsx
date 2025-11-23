d757448bf01e59ac129f5036e38fd27281d89fcd
Swaathi S M <smswaathi@gmail.com>
2025-11-23 09:32:44 +0530

    Added admin frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalendarEmbed from "./components/CalendarEmbed";
+import EventsAdminPage from "./components/EventsAdminPage";
+
+export default function App() {
+  return (
+    <div>
+      <EventsAdminPage />
+      <CalendarEmbed />
+    </div>
+  );
+}
+
